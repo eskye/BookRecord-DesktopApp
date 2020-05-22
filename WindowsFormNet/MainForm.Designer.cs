8ff9a4edccdb351665f3aaa7def984dba6b5d647
eskye <sunkanmiijatuyi@gmail.com>
2020-05-22 18:26:26 +0100

    initial commit

---
+++ WindowsFormNet/MainForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WindowsFormNet
+{
+    partial class BookRecordForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.delete_btn_nostock_books = new System.Windows.Forms.Button();
+            this.load_file_btn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToOrderColumns = true;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView.Location = new System.Drawing.Point(18, 130);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 25;
+            this.dataGridView.Size = new System.Drawing.Size(1046, 525);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1085, 51);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Book Records\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // delete_btn_nostock_books
+            // 
+            this.delete_btn_nostock_books.BackColor = System.Drawing.Color.Red;
+            this.delete_btn_nostock_books.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.delete_btn_nostock_books.FlatAppearance.BorderSize = 0;
+            this.delete_btn_nostock_books.Font = new System.Drawing.Font("Cambria", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delete_btn_nostock_books.ForeColor = System.Drawing.Color.White;
+            this.delete_btn_nostock_books.Location = new System.Drawing.Point(923, 15);
+            this.delete_btn_nostock_books.Name = "delete_btn_nostock_books";
+            this.delete_btn_nostock_books.Size = new System.Drawing.Size(108, 43);
+            this.delete_btn_nostock_books.TabIndex = 4;
+            this.delete_btn_nostock_books.Text = "Delete ";
+            this.delete_btn_nostock_books.UseVisualStyleBackColor = false;
+            this.delete_btn_nostock_books.Click += new System.EventHandler(this.delete_btn_nostock_books_Click);
+            // 
+            // load_file_btn
+            // 
+            this.load_file_btn.BackColor = System.Drawing.Color.DodgerBlue;
+            this.load_file_btn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.load_file_btn.FlatAppearance.BorderSize = 0;
+            this.load_file_btn.Font = new System.Drawing.Font("Cambria", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.load_file_btn.ForeColor = System.Drawing.Color.White;
+            this.load_file_btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.load_file_btn.Location = new System.Drawing.Point(807, 14);
+            this.load_file_btn.Name = "load_file_btn";
+            this.load_file_btn.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.load_file_btn.Size = new System.Drawing.Size(110, 44);
+            this.load_file_btn.TabIndex = 6;
+            this.load_file_btn.Text = "Load File";
+            this.load_file_btn.UseVisualStyleBackColor = false;
+            this.load_file_btn.Click += new System.EventHandler(this.load_file_btn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.load_file_btn);
+            this.groupBox1.Controls.Add(this.delete_btn_nostock_books);
+            this.groupBox1.Location = new System.Drawing.Point(18, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1046, 68);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // BookRecordForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 713);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "BookRecordForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Book Record";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button delete_btn_nostock_books;
+        private System.Windows.Forms.Button load_file_btn;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ WindowsFormNet/MainForm.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormNet
+{
+    public partial class BookRecordForm : Form
+    {
+        public const string Description = "description";
+        public const string Binding = "binding";
+        public const string InStock = "in stock";
+        public const string DescriptionButtonText = "Action";
+        public BookRecordForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void load_file_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var ppt = new OpenFileDialog { Filter = @"Comma Delimited Separated (.csv) | *.csv" };
+                if (ppt.ShowDialog() != DialogResult.OK) return;
+                var dataTable = ReadCsvFile(ppt.FileName);
+                dataGridView.DataSource = dataTable;
+                dataGridView.DataError += new DataGridViewDataErrorEventHandler(dgvCombo_DataError);
+                dataGridView.CellClick += new DataGridViewCellEventHandler(dataGridView1_CellClick);
+                dataGridView.ShowCellToolTips = true;
+                dataGridView.AllowUserToOrderColumns = false;
+                dataGridView.AllowUserToAddRows = false;
+                dataGridView.CellMouseEnter += DataGridView1_CellMouseEnter;
+
+                var col = new DataGridViewButtonColumn
+                {
+                    UseColumnTextForButtonValue = true,
+                    Text = "View Description",
+                    Name = DescriptionButtonText,
+                    
+                };
+                dataGridView.Columns.Add(col);
+                dataGridView.Columns[Description].Visible = false; 
+                ReplaceBindingColumnWithCombobox();
+
+                CheckIfInStock();
+
+                DisableSorting();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Error:" + exception.ToString());
+                throw;
+            }
+        }
+
+        
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            FormDesigner();
+        }
+
+        private void delete_btn_nostock_books_Click(object sender, EventArgs e)
+        {
+            var dialogResult = MessageBox.Show(@"Are you sure you want to perform this operation?", @"Prompt",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (dialogResult != DialogResult.OK) return;
+            if (!dataGridView.Columns.Contains(InStock))
+            {
+                MessageBox.Show(@"No record to delete", $@"Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+
+            }
+
+            var indexColumn = dataGridView.Columns.IndexOf(dataGridView.Columns[InStock]);
+
+            var totalRecordsDeleted = 0;
+            for (var i = 0; i < dataGridView.RowCount; i++)
+            {
+                var data = dataGridView.Rows[i].Cells[indexColumn];
+                if (data?.Value == null) continue;
+                var dvgValue = data.Value.ToString();
+                if (!dvgValue.Equals("no")) continue;
+                dataGridView.Rows.Remove(dataGridView.Rows[i]);
+                totalRecordsDeleted++;
+
+            }
+
+            MessageBox.Show($@"{totalRecordsDeleted} books are not in stock and has been deleted successfully",
+                $@"Delete Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+
+
+        #region EventHandlers
+
+        void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex < 0 || e.RowIndex < 0) return;
+            if (dataGridView.Columns[e.ColumnIndex].Name != DescriptionButtonText) return;
+            var descriptionColumn = dataGridView.Columns.IndexOf(dataGridView.Columns[Description]);
+
+            var description = dataGridView.Rows[e.RowIndex].Cells[descriptionColumn].Value.ToString();
+            // button clicked - do some logic
+            MessageBox.Show(description, @"Description", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        void dgvCombo_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            // (No need to write anything in here)
+        }
+        private void DataGridView1_CellMouseEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex < 0 || e.RowIndex < 0) return;
+            if (dataGridView.Columns[e.ColumnIndex].Name != DescriptionButtonText) return;
+            var descriptionColumn = dataGridView.Columns.IndexOf(dataGridView.Columns[Description]);
+            var descriptionBtnCell = dataGridView.Rows[e.RowIndex].Cells[e.ColumnIndex];
+            var data = dataGridView.Rows[e.RowIndex].Cells[descriptionColumn];
+            if (data?.Value == null) return;
+            var description = data.Value.ToString();
+            descriptionBtnCell.ToolTipText = description;
+        }
+        #endregion
+
+
+        #region HelperFunctions
+        private static DataTable ReadCsvFile(string csvFilePath)
+        {
+            var lines = File.ReadAllLines(csvFilePath);
+            var fields = lines[0].Split(new char[] { ';' });
+            var cols = fields.GetLength(0);
+            var dt = new DataTable();
+            for (var i = 0; i < cols; i++)
+            {
+                if (fields[i].Contains("Description,,,,,,,,,"))
+                    fields[i] = Description;
+                dt.Columns.Add(fields[i].ToUpper(), typeof(string));
+            }
+
+            for (var i = 1; i < lines.GetLength(0); i++)
+            {
+                fields = lines[i].Split(new char[] { ';' });
+                var row = dt.NewRow();
+                for (var f = 0; f < cols; f++)
+                    if (!string.IsNullOrEmpty(fields[f])) row[f] = fields[f];
+                dt.Rows.Add(row);
+            }
+
+            return dt;
+        }
+
+        private void FormDesigner()
+        {
+            dataGridView.BorderStyle = BorderStyle.None;
+            dataGridView.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dataGridView.RowsDefaultCellStyle.Padding = new Padding(20, 5, 0, 0);
+            dataGridView.DefaultCellStyle.SelectionForeColor = Color.WhiteSmoke;
+
+            dataGridView.EnableHeadersVisualStyles = false;
+            dataGridView.AllowUserToOrderColumns = false;
+            dataGridView.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridView.ColumnHeadersDefaultCellStyle.BackColor = Color.DodgerBlue;
+            dataGridView.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dataGridView.ColumnHeadersDefaultCellStyle.Font = new Font("Cambria", 8.5F, FontStyle.Bold);
+            dataGridView.ColumnHeadersDefaultCellStyle.Padding = new Padding(0, 10, 0, 10);
+        }
+
+       
+        private void CheckIfInStock()
+        {
+            if (!dataGridView.Columns.Contains(InStock)) return;
+            var indexColumn = dataGridView.Columns.IndexOf(dataGridView.Columns[InStock]);
+
+            for (var i = 0; i < dataGridView.RowCount; i++)
+            {
+                var combo = new DataGridViewCheckBoxCell();
+
+                var data = dataGridView.Rows[i].Cells[indexColumn];
+                if (data?.Value == null) continue;
+                var dvgValue = data.Value.ToString();
+                if (dvgValue.Equals("no"))
+                {
+                    combo.Value = 1;
+                    dataGridView.Rows[i].DefaultCellStyle.BackColor = Color.GhostWhite;
+                    dataGridView.Rows[i].DefaultCellStyle.ForeColor = Color.DarkRed;
+
+                }
+                else if (dvgValue.Equals("yes"))
+                {
+                    combo.Value = 0;
+
+                }
+
+                dataGridView.Rows[i].Cells[indexColumn] = combo;
+
+            }
+
+        }
+
+        private void ReplaceBindingColumnWithCombobox()
+        {
+            if (!dataGridView.Columns.Contains(Binding)) return;
+            var indexColumn = dataGridView.Columns.IndexOf(dataGridView.Columns[Binding]);
+
+            for (var i = 0; i < dataGridView.RowCount; i++)
+            {
+                var combo = new DataGridViewComboBoxCell();
+
+                var comboSource = new Dictionary<int, string>();
+                var data = dataGridView.Rows[i].Cells[indexColumn];
+                if (data?.Value == null) continue;
+                var dvgValue = data.Value.ToString();
+                comboSource.Add(i, dvgValue);
+                combo.DataSource = new BindingSource(comboSource, null);
+                combo.DisplayMember = "Value";
+                combo.ValueMember = "Key";
+
+                dataGridView.Rows[i].Cells[indexColumn] = combo;
+
+            }
+        }
+
+        private void DisableSorting()
+        {
+            foreach (DataGridViewColumn column in dataGridView.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+        }
+
+
+
+        #endregion
+
+         
+    }
+}
